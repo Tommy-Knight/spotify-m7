2e1018f41057380d03b2822953251c349ad94631
gregoriofracassi <gregorio.fracassi@gmail.com>
2021-06-25 16:33:01 +0200

    comm

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,12 @@ export const initialState = {
   },
   currentPlaylist: {
     songs: [],
-    currentSong: {},
+    currentSong: {
+      title: "",
+      artist: {
+        name: "",
+      },
+    },
     cover: "",
   },
 }
